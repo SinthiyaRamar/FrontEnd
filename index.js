2fbc7e12b261476add564c061ae71f6fe6afaf89
SinthiyaRamar <sinthiyasinthiya249@gmail.com>
2022-12-08 11:28:28 +0530

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+Array.prototype.SuffleArray=function(){
+    let array=this;
+    
+    for(let i=array.length-1; i>=0; i--){
+      let randomPosition=Math.floor(Math.random()*i);
+      let currentPostion=array[i];
+         array[i]=array[randomPosition];
+         array[randomPosition]=currentPostion;
+    }
+    
+    console.log(array);
+    return array;
+  }
+  
+  let arr=[1,2,3,4,5,5];
+  arr.SuffleArray();
+
+  
